9a25bc2dbce621b1dc59373d820f651e2605e8af
mlodszyrower <44478781+mlodszyrower@users.noreply.github.com>
2020-05-16 12:34:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Simple_calculator
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            double firstNumber;
+            double secondNumber;
+            string op;
+            double result;
+
+            #region firstNumber
+            Console.WriteLine("First number");
+            firstNumber = Double.Parse(Console.ReadLine());
+            #endregion
+
+            #region operator
+            Console.WriteLine("Operator");
+            op = Console.ReadLine();
+            #endregion
+
+            #region secondNumber
+            Console.WriteLine("Second number");
+            secondNumber = Double.Parse(Console.ReadLine());
+            #endregion
+
+            #region result
+            if(op == "+")
+            {
+                result = firstNumber + secondNumber;
+                Console.WriteLine(result);
+            }
+            else if (op == "-")
+            {
+                result = firstNumber - secondNumber;
+                Console.WriteLine(result);
+            }
+            else if (op == "*")
+            {
+                result = firstNumber * secondNumber;
+                Console.WriteLine(result);
+            }
+
+            else if (op == "/")
+            {
+                result = firstNumber / secondNumber;
+                Console.WriteLine(result);
+            }
+            #endregion
+
+            Console.ReadKey();
+
+        }
+    }
+}
